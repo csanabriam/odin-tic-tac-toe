b2b9e8f11ba8db634ca598646e8fa860fe0a91df
Camilo <114685852+csanabriam@users.noreply.github.com>
2022-12-28 15:55:41 -0500

    Comment the code.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const tictactoe = (() => {
+    // Contains all submodules and functions to control the game and returns the function to initialize it.
 
     const factorial = (n) => {
+        // return n!
         return (n > 1) ? n * factorial(n-1) : 1;
     }
 
     const getAllIndexes = (arr, val) => {
+        // return all the indexes of arr where val appears
         var indexes = [], i;
         for(i = 0; i < arr.length; i++)
             if (arr[i] === val)
@@ -13,19 +16,23 @@ const tictactoe = (() => {
     }
 
     const gameboard = (() => {
-        let grid = [['','',''],['','',''],['','','']],
-            turnOfPlayer = 0,
+        // Contains the functions to control the gameboard. 
+        let grid = [['','',''],['','',''],['','','']], //the gameboard is a 3x3 matrix
+            turnOfPlayer = 0, //0 for player1 and 1 for player2
             thereIsAWinner = false,
             turn = 0;
 
-
+        //Array 'spaces' contains the html elements to mark the gameboard
         const spaces = document.querySelectorAll(".space");
 
         const _changePlayer = () => {
+            // Change the turn counter.
+            // This function is called after a player plays.
             turnOfPlayer = turnOfPlayer ? 0 : 1;
         }
 
         const checkIfTurnOfCPU = () => {
+            // Determine whether it's the CPU turn to play and if so calls the function to make the CPU play.
             if (turnOfPlayer === 0 && game.player1.isCPU) {
                 setTimeout(_cpuPlays,1000)
             } else if (turnOfPlayer === 1 && game.player2.isCPU) {
@@ -33,11 +40,9 @@ const tictactoe = (() => {
             }
         }
 
-        const checkIfSpaceIsMarked = ([row,column]) => {
-            return grid[row][column] ? true : false;
-        }
-
         const _play = (event) => {
+            // Mark an empty space with O or X and, if there is no winner, hands the turn to the other player.
+            // This function is called when a player clicks in his turn on an empty space.
             let row = +event.composedPath()[0].getAttribute("row"),
                 column = +event.composedPath()[0].getAttribute("column");
             if (!(grid[row][column])) {
@@ -55,6 +60,8 @@ const tictactoe = (() => {
         }
 
         const _cpuPlays = () => {
+            // Mark an empty space with O or X, based on the AI strategy, and, if there is no winner, hands the turn to the other player.
+            // This function is called to make the CPU play in its turn.
             if (!thereIsAWinner) {
                 let [row, column] = ai.cpuPlays(turnOfPlayer, turn);
                 if (!(grid[row][column])) {
@@ -73,6 +80,7 @@ const tictactoe = (() => {
         }
 
         const _addClickListeners = () => {
+            // Add click listener to the gameboard spaces
             for (let space = 0; space < 9; space++) {
                 let column = space % 3,
                     row = (space - column)/3;
@@ -83,12 +91,15 @@ const tictactoe = (() => {
         }
 
         const markWinningSpaces = (toMark) => {
+            // Add the class 'won' to an html element in the gameboard.
+            // This function is called when a space is part of a three in line.
             for (let [row,column] of toMark) {
                 spaces[row*3+column].classList.toggle('won');
             }
         }
 
         const removeClickListeners = () => {
+            // Remove the click listeners to the gameboard spaces.
             for (let space = 0; space < 9; space++) {
                 let column = space % 3,
                     row = (space - column)/3;
@@ -97,6 +108,7 @@ const tictactoe = (() => {
         }
 
         const restartGrid = () => {
+            // Erase marks in the gameboard, and restart the html elements of the gameboard spaces 
             grid = [['','',''],['','',''],['','','']];
             turnOfPlayer = 0;
             for (let space = 0; space < 9; space++) {
@@ -109,10 +121,12 @@ const tictactoe = (() => {
         }
 
         const returnGrid = () => {
+            // Return the gameboard to be read by other modules.
             return grid;
         }
 
         const checkIfFull = () => {
+            // Check if the gameboard is full.
             let isFull = true;
 
             for (let space = 0; space < 9; space++) {
@@ -126,13 +140,15 @@ const tictactoe = (() => {
             return isFull;
         }
 
-        return {markWinningSpaces, removeClickListeners, restartGrid, checkIfTurnOfCPU, checkIfSpaceIsMarked, returnGrid, checkIfFull};
+        return {markWinningSpaces, removeClickListeners, restartGrid, checkIfTurnOfCPU, returnGrid, checkIfFull};
     })();
 
     const checkWinner = (() =>  {
-        let winner = 0; 
+        // This module contains the functions to determine whether there is a winner.
+        let winner = 0; //0 for no winner, 1 if player1 is the winner, 2 if it's player2
 
         const _checkRows = (grid, turnOfPlayer) => {
+            // Check if there are 3 of the same kind in a single row.
             for (let i = 0; i < 3; i++) {
                 if (grid[i][0]) {
                     if ((grid[i][0] === grid[i][1]) && (grid[i][0] === grid[i][2])) {
@@ -145,6 +161,7 @@ const tictactoe = (() => {
         }
         
         const _checkColumns = (grid, turnOfPlayer) => {
+            // Check if there are 3 of the same kind in a single column.
             for (let j = 0; j < 3; j++) {
                 if (grid[0][j]) {
                     if ((grid[0][j] === grid[1][j]) && (grid[0][j] === grid[2][j])) {
@@ -157,6 +174,7 @@ const tictactoe = (() => {
         }
 
         const _checkDiagonals = (grid, turnOfPlayer) => {
+            // Check if there are 3 of the same kind in a single diagonal.
             if (grid[0][0]) {
                 if ((grid[0][0] === grid[1][1]) && (grid[0][0] === grid[2][2])) {
                     winner = turnOfPlayer+1;
@@ -172,6 +190,7 @@ const tictactoe = (() => {
         }
 
         const checkIfWinner = (grid, turnOfPlayer) => {
+            // Check if there is a winner
             if (!winner) {
                 _checkRows(grid, turnOfPlayer);
             };
@@ -191,6 +210,7 @@ const tictactoe = (() => {
         }
 
         const restartWinner = () => {
+            // Reset the winner counter.
             winner = 0;
         }
 
@@ -198,12 +218,14 @@ const tictactoe = (() => {
     })();
 
     const display = (() => {
+        // Contains the functions to controls the html elements of the display: the names of the players and the sign announcing the winner.
         const headerForm = document.querySelector(".header");
         const player1Name = document.querySelector("#player1");
         const player2Name = document.querySelector("#player2");
         const display = document.querySelector(".display");
 
         const _setPlayersNames = () => {
+            // Initialize the display containing the names of the players and add listeners to it.
             player1Name.value = game.player1.name;
             player2Name.value = game.player2.name;
             headerForm.onkeydown = (event) => {
@@ -215,6 +237,7 @@ const tictactoe = (() => {
         }
 
         const _setNewPlayersNames = () => {
+            // Read the names of the players, determine whether one of them is the CPU and if so set the difficulty level.
             const names = new FormData(headerForm);
             game.player1.name = names.get("player1");
             game.player2.name = names.get("player2");
@@ -250,6 +273,7 @@ const tictactoe = (() => {
         }
 
         const displayWinner = (winner) => {
+            // Display the winner's name.
             if (winner === 1) {
                 display.textContent = `${game.player1.name} wins!`
             } else {
@@ -258,6 +282,7 @@ const tictactoe = (() => {
         }
 
         const resetDisplay = () => {
+            // Reset the display.
             display.textContent = '';
             _setPlayersNames();
             _setNewPlayersNames();
@@ -267,21 +292,21 @@ const tictactoe = (() => {
     })();
 
     const playerFactory = (name) => {
+        // Create a player object.
         let isCPU = false,
             CPUlevel = 0;
         return {name, isCPU, CPUlevel};
     };
 
     const ai = ( () => {
-
-        const _randomSpace = () => {
-            return Math.floor(9*Math.random());
-        }
+        // Contains the AI for CPU.
 
         const cpuPlays = (turnOfPlayer, turn) => {
+            // Make the CPU play based on whether it's playing randomly, optimally or in default mode.
             let player = turnOfPlayer? 'player2' : 'player1';
             switch (game[player].CPUlevel) {
                 case 1:
+                    // The CPU plays randomly except when he can make a winning choice.
                     let possibleRandomDecisions = [],
                         copyOfGrid = Array.from(gameboard.returnGrid());
 
@@ -294,6 +319,7 @@ const tictactoe = (() => {
                     }
 
                     for (let space of possibleRandomDecisions) {
+                        // Checks if there is a winning choice and if so, chooses it.
                         let column = space % 3,
                             row = (space - column)/3,
                             newPhantomGrid = [];
@@ -313,6 +339,7 @@ const tictactoe = (() => {
                     return [randomRow,randomColumn];
                 break;
                 case 3:
+                    // The CPU plays so that it minimizes the chances of loosing according to the weights assigned to each possible choice by the function weightPossibleMoves2. It's an optimal choice.
                     let [possibleDecisions1, decisionsDamage1, decisionsAdvantage1] =  ai.weightPossibleMoves(Array.from(gameboard.returnGrid()),turnOfPlayer,turnOfPlayer,turn),
                         [possibleDecisions2, decisionsDamage2] =  ai.weightPossibleMoves2(Array.from(gameboard.returnGrid()),turnOfPlayer,turnOfPlayer,turn),
                         min2 = Math.min(...decisionsDamage2),
@@ -329,6 +356,7 @@ const tictactoe = (() => {
                     return [optimalRow, optimalColumn];
                     break;
                 default:
+                    //  The CPU plays so that it minimizes the chances of loosing according to the weights assigned to each possible choice by the function weightPossibleMoves.
                     let [possibleDecisions, decisionsDamage, decisionsAdvantage] =  ai.weightPossibleMoves(Array.from(gameboard.returnGrid()),turnOfPlayer,turnOfPlayer,turn),
                         min = Math.min(...decisionsDamage),
                         bestDecisions = getAllIndexes(decisionsDamage,min),
@@ -341,13 +369,12 @@ const tictactoe = (() => {
             }
         }
 
-        let thereIsAPhantomWinner = false,
-            turnOfPhantomPlayer = 0;
-
         const phantomGridProto = {
-            phantomDepth : 0,
+            // Create a phantom gameboard to use in making possible scenarios. 
+            phantomDepth : 0, // Keep a count of the turn in which the possible scenario happens.
             
             setDepth() {
+                // Count in which turn the possible scenario is.
                 for (let space = 0; space < 9; space++) {
                     let column = space % 3,
                         row = (space - column)/3;
@@ -358,11 +385,13 @@ const tictactoe = (() => {
             },
 
             phantomPlay ([row, column]) {
+                // Mark a space in the phantom gameboard and changes player.
                 this.grid[row][column] = this.phantomTurnOfPlayer ? "O" : "X";
                 this.phantomTurnOfPlayer = this.phantomTurnOfPlayer ? 0 : 1;
             },
 
             checkIfPhantomWinner () {
+                // Check whether there is a winner in the phantom gameboard.
                 for (let i = 0; i < 3; i++) {
                     if (this.grid[i][0]) {
                         if ((this.grid[i][0] === this.grid[i][1]) && (this.grid[i][0] === this.grid[i][2])) {
@@ -395,6 +424,7 @@ const tictactoe = (() => {
             },
 
             checkIfFull () {
+                // Check whether the phantom gameboard is full.
                 let isFull = true;
 
                 for (let space = 0; space < 9; space++) {
@@ -410,6 +440,7 @@ const tictactoe = (() => {
         };
 
         const phantomPlays = (grid, requester, phantomTurnOfPlayer) => {
+            // Get all possible ramifications from a given phantom gameboard. It's an inductive function that calls itself after making a choice to get all possible ramification from it.
             let children = [];
             
             for (let space = 0; space < 9; space++) {
@@ -435,6 +466,7 @@ const tictactoe = (() => {
         }
 
         const weightPossibleMoves = (grid, requester, phantomTurnOfPlayer, turnNumber) => {
+            // Calculate how likely is that you could loose or win for each single choice available in a phantom gameboard.
             let possibleDecisions = [],
                 decisionsDamage = [],
                 decisionsAdvantage = [];
@@ -476,6 +508,7 @@ const tictactoe = (() => {
         }
 
         const weightPossibleMoves2 = (grid, requester, phantomTurnOfPlayer, turnNumber) => {
+            // Calculate how likely is that you could loose for each single choice available in a given scenario, assuming that after this choice each player minimizing his chances of loosing and then maximizes his chances of winning according to the weights assigned in weightPossible Moves.
             let possibleDecisions = [],
                 decisionsDamage = [],
                 opponent = phantomTurnOfPlayer ? 0 : 1;
@@ -525,6 +558,7 @@ const tictactoe = (() => {
     })()
 
     const game = ( () => {
+        // Contains the game controls: create the players and starts and stops the game.
         const player1 = playerFactory("Player 1");
         const player2 = playerFactory("Player 2");
 
